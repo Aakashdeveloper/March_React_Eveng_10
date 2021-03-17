8be72cd988bb981174c72f72dd68676b4887f0f9
aakashdeveloper <ahanda205@gmail.com>
2021-03-17 22:36:44 +0530

    appgenerated

---
+++ firstapp/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(){
+    return(
+        <h1>Say Hi To React</h1>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
