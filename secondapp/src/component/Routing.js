c5de011430ed1bcb6e17a14c3b35d4330556c109
aakashdeveloper <ahanda205@gmail.com>
2021-03-23 22:50:24 +0530

    A

---
+++ secondapp/src/component/Routing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import Post from './Post';
+import Profile from './Profile';
+import Header from './Header';
+import Footer from './Footer'
+import PostDetails from './PostDetails';
+
+const Routing = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/post" component={Post}/>
+                <Route path="/post/:topic" component={PostDetails}/>
+                <Route path="/profile" component={Profile}/>
+                <Footer/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default Routing
