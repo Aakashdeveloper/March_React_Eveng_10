16a1f263c1c960b175f23ce50c50819105c80c71
aakashdeveloper <ahanda205@gmail.com>
2021-03-25 22:51:12 +0530

    listing

--- thirdapp/src/component/Home/QuickSearch.js
+++ thirdapp/src/component/Home/QuickSearch.js
@@ -1,12 +1,30 @@
 import React,{Component} from 'react';
+import QuickDisplay from './QuickDisplay';
+
+const url = "https://developerfunnel.herokuapp.com/booking";
 
 class QuickSearch extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            tripType:''
+        }
+    }
     render(){
         return(
             <div>
-                <h1>QuickSearch Page</h1>
+                <QuickDisplay tripdata={this.state.tripType}/>
             </div>
         )
+    }
+
+    componentDidMount(){
+        fetch(url,{method:'GET'})
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({tripType:data})
+        })
     }
 }
 
